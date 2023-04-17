7a13aa5744436165eeaba66a4bc3567e792d83f1
jonaaldas <jonaaldas@gmail.com>
2023-04-17 17:57:01 -0400

    working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 	const [token, setToken] = useState('');
 	const [tranReady, setTranReady] = useState(false);
 	const [banks, setBanks] = useState([]);
+	let link = 'http://localhost:9000/api';
 
 	const generateLinkToken = async () => {
 		const response = await fetch(
@@ -21,44 +22,54 @@ function App() {
 		setToken(data.link_token);
 	};
 
-	const onSuccess = async (/** @type {any} */ token) => {
-		// let bankDetails = await axios.post('http://localhost:9000/api/item/get');
+	const onSuccess = async (/** @type {any} */ token, metadata) => {
 		let accessToken = await axios.post(
 			'http://localhost:9000/api/set_access_token',
 			{
 				data: token,
+				metadata: metadata,
 			}
 		);
-		// console.log(bankDetails);
-		// const jsonObject = JSON.parse(accessToken.data);
-		// console.log(jsonObject);
-		console.log(accessToken);
-
-		// setBanks((prev) => {
-		// 	return [...prev, accessToken.data];
-		// });
+		console.log(
+			'🚀 ~ file: App.jsx:32 ~ onSuccess ~ accessToken:',
+			accessToken
+		);
 	};
 
-	const getTransactions = async () => {
-		const { data } = await axios.post('http://localhost:9000/api/transactions');
-
-		console.log('🚀 ~ file: App.jsx:39 ~ getTransactions ~ data:', data);
+	const init = () => {
+		// gets all transactions from the last day and all balances for the last 24H
+		axios
+			.get(`${link}/get/allInformation`)
+			.then((res) => console.log(res))
+			.catch((error) => console.log(error));
 	};
 
-	const getBankAccounts = async () => {
-		await axios
-			.post('http://localhost:9000/api/accounts/get', {
-				data: banks[0].access_token,
-			})
-			.then((data) => console.log(data));
+	const getBankNameFromDB = async () => {
+		let { data } = await axios.get(`${link}/get/bankInformation`);
+		setBanks(data);
 	};
 
-	const getInstitutionInfo = async () => {
-		await axios
-			.post('http://localhost:9000/api/item/get')
-			.then((res) => res)
-			.catch((e) => console.log(e));
-	};
+	// const getTransactions = async () => {
+	// 	const { data } = await axios.post('http://localhost:9000/api/transactions');
+	// 	console.log('I am running');
+
+	// 	console.log('🚀 ~ file: App.jsx:39 ~ getTransactions ~ data:', data);
+	// };
+
+	// const getBankAccounts = async () => {
+	// 	await axios
+	// 		.post('http://localhost:9000/api/accounts/get', {
+	// 			data: banks[0].access_token,
+	// 		})
+	// 		.then((data) => console.log(data));
+	// };
+
+	// const getInstitutionInfo = async () => {
+	// 	await axios
+	// 		.post('http://localhost:9000/api/item/get')
+	// 		.then((res) => res)
+	// 		.catch((e) => console.log(e));
+	// };
 
 	let isOauth = false;
 	const config = {
@@ -67,11 +78,14 @@ function App() {
 	};
 
 	const { open, ready } = usePlaidLink(config);
-
 	useEffect(() => {
 		generateLinkToken();
 		open();
 	}, []);
+
+	useEffect(() => {
+		getBankNameFromDB();
+	}, [banks]);
 
 	return (
 		<div className='App'>
@@ -83,11 +97,10 @@ function App() {
 			>
 				Connect a bank account
 			</button>
-			<button onClick={() => getTransactions()}>Get my transactions</button>
-			<button onClick={() => getBankAccounts()}>Get my account info</button>
-			<button onClick={() => getInstitutionInfo()}>Get my Bank Info</button>
 
-			<BankCard />
+			<button onClick={init}>INIT</button>
+
+			<BankCard bankName={{ banks }} />
 		</div>
 	);
 }
--- client/src/components/BankCard.jsx
+++ client/src/components/BankCard.jsx
@@ -2,12 +2,22 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-function BankCard() {
+function BankCard({ bankName }) {
 	return (
 		<div className='max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
 			<h1 className='mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-3xl dark:text-white'>
 				We invest in the world’s potential
 			</h1>
+			{bankName.banks.map((bank, index) => {
+				return (
+					<span
+						className='bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300'
+						key={index}
+					>
+						{bank.bank_name}
+					</span>
+				);
+			})}
 		</div>
 	);
 }
